e1d250112f0597489dad9270db347997a875338a
Chhatrapal <chhatrapalgangurde246@gmail.com>
2023-07-20 14:31:26 +0530

    Added message in AppConstant

--- src/main/java/com/bikkadit/store/Constant/AppConstant.java
+++ src/main/java/com/bikkadit/store/Constant/AppConstant.java
@@ -4,7 +4,7 @@ public class AppConstant
 {
     public static final String USER_NOT_FOUND = "User Not Found With Given ID.";
     public static final String EMAIL_NOT_FOUND = "User Not Found With Given Email Id ..!!" ;
-    public static final String EXTENSION = "Extension Not Allowed." ;
+    public static final String EXTENSION = "Extension Not Allowed ." ;
     public static final String IMAGE_UPLOAD = "Image Upload Successfully." ;
     public static final String DELETE_USER = "User Deleted Successfully." ;
     public  static final String CATEGORY_NOT_FOUND = "Category not found with given category Id";
