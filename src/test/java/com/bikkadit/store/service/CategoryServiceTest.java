01dd1f7c836dd686182fc5fa6e7321f4930150bd
Chhatrapal <chhatrapalgangurde246@gmail.com>
2023-08-04 08:21:46 +0530

    Test case implementation for updateCategory() done

--- src/test/java/com/bikkadit/store/service/CategoryServiceTest.java
+++ src/test/java/com/bikkadit/store/service/CategoryServiceTest.java
@@ -1,6 +1,7 @@
 package com.bikkadit.store.service;
 
 import com.bikkadit.store.dto.CategoryDto;
+import com.bikkadit.store.dto.PageableResponse;
 import com.bikkadit.store.dto.UserDto;
 import com.bikkadit.store.entity.Category;
 import com.bikkadit.store.entity.User;
@@ -16,9 +17,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
-import java.util.Locale;
-import java.util.Optional;
+import java.util.*;
 
 @SpringBootTest
 public class CategoryServiceTest
@@ -54,5 +57,68 @@ public class CategoryServiceTest
         Assertions.assertNotNull(category1);
         Assertions.assertEquals("Desktop",category1.getTitle());
     }
+    @Test
+    public void updateCategory()
+    {
+        String categoryId = "hasdfvjyehf";
+        CategoryDto categoryDto = CategoryDto.builder()
+                .title("Laptop")
+                .description("This is updated user details")
+                .coverImage("xyz.png")
+                .build();
+        Mockito.when(categoryRepository.findById(Mockito.anyString())).thenReturn(Optional.of(category));
+        Mockito.when(categoryRepository.save(Mockito.any())).thenReturn(category);
 
+        CategoryDto updateUser = categoryService.update(categoryDto, categoryId);
+        System.out.println(updateUser.getTitle());
+
+        Assertions.assertNotNull(updateUser);
+        Assertions.assertEquals(updateUser.getTitle(),updateUser.getTitle(),"Name is not Valid");
+    }
+
+    @Test
+    public void deleteCategory()
+    {
+        String categoryId = UUID.randomUUID().toString();
+        Mockito.when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(category));
+        categoryService.delete(categoryId);
+        Mockito.verify(categoryRepository,Mockito.times(1)).delete(category);
+    }
+
+    @Test
+    public void getAllCategory()
+    {
+        Category category1 = Category.builder()
+                .title("Laptop")
+                .description("This is testing create method")
+                .coverImage("abc.png")
+                .build();
+        Category category2 = Category.builder()
+                .title("Laptop")
+                .description("This is testing create method")
+                .coverImage("abc.png")
+                .build();
+
+        List<Category> categoryList = Arrays.asList(category,category1,category2);
+        Page<Category> page = new PageImpl<>(categoryList);
+        Mockito.when(categoryRepository.findAll((Pageable) Mockito.any())).thenReturn(page);
+
+        //Sort sort = Sort.by("name").ascending();
+        //Pageable pageable = PageRequest.of(1,2,sort);
+        PageableResponse<CategoryDto> allUser = categoryService.getAllCategory(1,2,"name","asc");
+        Assertions.assertEquals(3,allUser.getContent().size());
+    }
+
+    @Test
+    public void getCategoryById()
+    {
+        String categoryId = "d7da596d-2a1c-4292-be7d-4f3a71e65f60";
+
+        Mockito.when(categoryRepository.findById(Mockito.anyString())).thenReturn(Optional.of(category));
+        // actual call of service method
+        CategoryDto categoryDto = categoryService.getCategory(categoryId);
+
+        Assertions.assertNotNull(categoryDto);
+        Assertions.assertEquals(category.getTitle(),categoryDto.getTitle(),"Title not matched");
+    }
 }
